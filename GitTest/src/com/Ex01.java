38d732b1a8265a32d95e0245a5a597794a0daa1e
BSW <bsw797@gmail.com>
2026-02-25 15:46:33 +0900

    여섯번째 커밋

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -7,7 +7,8 @@ public class Ex01 {
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀원2 세번째 커밋");
 		System.out.println("팀장 네번째 커밋");
-		System.out.println("팀원1 세번째 커밋");
+		System.out.println("팀원1 다섯번째 커밋");
+		System.out.println("팀원2 여섯번째 커밋");
 	}
 
 }
